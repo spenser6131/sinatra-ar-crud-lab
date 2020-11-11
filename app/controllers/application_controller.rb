0a6a3580a3764229ae4971e9d8032a44addfcdc0
spenser6131 <brinkman.spenser@gmail.com>
2020-11-11 15:02:03 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,54 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/articles'
   end
+
+  # index
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  # new
+  get '/articles/new' do
+    erb :new
+  end
+
+  # create
+  post '/articles' do
+    article = Article.create(params)
+    redirect to "/articles/#{article.id}"
+  end
+
+  # show
+  get '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    if @article
+      erb :'/show'
+    else
+      "Woops! That article doesn't exist."
+    end
+  end
+
+  # edit
+  get '/articles/:id/edit' do
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  # update
+  patch '/articles/:id' do
+    article = Article.find_by_id(params[:id])
+    article.update(params[:article])
+    redirect to "/articles/#{article.id}"
+  end
+
+  # destroy
+  delete '/articles/:id' do
+    article = Article.find_by_id(params[:id])
+    Article.destroy(params[:id])
+    redirect to '/articles'
+  end
+
 end
